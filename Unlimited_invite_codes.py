c5189e6119a1624febdb6ef049116e14feeec966
Bhaskar Pal <bhaskarpal347@gmail.com>
2021-02-17 12:23:06 +0530

    Update Unlimited_invite_codes.py

--- Unlimited_invite_codes.py
+++ Unlimited_invite_codes.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
 import requests,base64,json
 url = "https://www.hackthebox.eu/api/invite/generate"
 num = int(input('Enter the number of invite codes: '))
 invites=[]
 for i in range(0,num):
-        headers = {'User-Agent': ''}
-        result = requests.post(url,headers=headers)  
-        data = json.loads(result.text)
-        parse = data['data']['code']
-        decodedBytes =(base64.b64decode(parse))
+        head = {'User-Agent': ''}
+        out = requests.post(url,headers=head)  
+        info = json.loads(out.text)
+        result = info['data']['code']
+        decodedBytes =(base64.b64decode(result))
         decodedStr = str(decodedBytes, "utf-8")
         invites.append(decodedStr)
 count=1
