11f58bb5c553815d59452d8bc84306ee51b07a48
Bhaskar Pal <bhaskarpal347@gmail.com>
2021-02-18 04:03:47 +0530

    Update HTB_Invite_code_generator.py

--- HTB_Invite_code_generator.py
+++ HTB_Invite_code_generator.py
@@ -1,10 +1,15 @@
+import pyfiglet
+from termcolor import colored
+CRED = '\033[91m'
+CEND = '\033[0m'
 import requests,base64,json
 url = "https://www.hackthebox.eu/api/invite/generate"
+print(CRED+pyfiglet.figlet_format('HTB Invite Code Generator')+CEND)
 num = int(input('Enter the number of invite codes: '))
 invites=[]
 for i in range(0,num):
         head = {'User-Agent': ''}
-        out = requests.post(url,headers=head)  
+        out = requests.post(url,headers=head)
         info = json.loads(out.text)
         result = info['data']['code']
         decodedBytes =(base64.b64decode(result))
